4016acf4beaf71a3fd8c2073612d0acfa31a7726
Amber Jones <amberlovescats14@gmail.com>
2020-03-02 21:01:21 -0600

    first commit

---
+++ Mosh_Part2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mosh_Part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
